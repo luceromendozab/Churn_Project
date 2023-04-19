36cc0e5c7075302741b68f46c4d6bd2a84d29b47
luceromendozab <luceromendoz@gmail.com>
2023-04-19 19:52:05 +0200

    str2

--- streamlit/src/soporte.py
+++ streamlit/src/soporte.py
@@ -2,3 +2,11 @@ import pickle
 import pandas as pd
 import streamlit as st
 
+# encoding Contract 
+with open('data/ML/pkl/encodingContract.pkl', 'rb') as contract:
+    encoding_contract = pickle.load(contract)
+
+# encoding Device Protection 
+with open('data/ML/pkl/encodingDevice Protection Plan.pkl', 'rb') as contract:
+    encoding_contract = pickle.load(contract)
+
